26339dc14abdcd990080c84bd4778fed0079c99e
danielriley06 <driley3@gmail.com>
2016-09-03 01:27:00 -0400

    heroku init

---
+++ app/models/seek.rb
@@ -0,0 +1,24 @@
+class Seek < ApplicationRecord
+  def self.create(uuid)
+    if !REDIS.get("matches").blank?
+      # Get the uuid of the player waiting
+      opponent = REDIS.get("matches")
+
+      Game.create(uuid, opponent)
+      # Clear the waiting key as no one new is waiting
+      REDIS.set("matches", nil)
+    else
+      REDIS.set("matches", uuid)
+    end
+  end
+
+  def self.remove(uuid)
+    if uuid == REDIS.get("matches")
+      REDIS.set("matches", nil)
+    end
+  end
+
+  def self.clear_all
+    REDIS.del("matches")
+  end
+end
