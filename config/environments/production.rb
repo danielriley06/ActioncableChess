cf025e75767a779f1e36af5c2c8cd8fc190755d5
danielriley06 <driley3@gmail.com>
2016-09-03 01:27:49 -0400

    heroku init fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-   config.action_cable.url = 'wss://agile-savannah-67368.herokuapp.com/cable'
-   config.action_cable.allowed_request_origins = [ 'http://agile-savannah-67368.herokuapp.com', /http:\/\/agile-savannah-67368.herokuapp.com.*/ ]
+   config.action_cable.url = 'wss://safe-springs-70759.herokuapp.com/cable'
+   config.action_cable.allowed_request_origins = [ 'http://safe-springs-70759.herokuapp.com', /http:\/\/safe-springs-70759.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
